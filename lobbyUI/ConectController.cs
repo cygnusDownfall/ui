dd4f5be122d6bed7e37bad5c20cfcc48fb47defc
cygnusDownfall <phapsuhacam112@gmail.com>
2023-09-03 12:48:51 +0700

    before arch

---
+++ lobbyUI/ConectController.cs
@@ -0,0 +1,77 @@
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+public class ConectController : MonoBehaviour
+{
+    [SerializeField] int maxPlayer = 4;
+    async void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        await UnityServices.InitializeAsync();
+
+    }
+    async void Start()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync().ContinueWith(task =>
+                        {
+                            if (task.IsFaulted)
+                            {
+                                Debug.LogError("Failed to sign in anonymously: " + task.Exception);
+                            }
+                            else
+                            {
+                                Debug.Log("Signed in anonymously");
+                            }
+                        });
+
+        }catch(AuthenticationException e){
+            Debug.Log("Authen error: " + e);
+        }
+
+        //
+    }
+
+    public async Task<string> createRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxPlayer);
+            string joincode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            RelayServerData sd = new RelayServerData(allocation, "dtls");
+            GetComponent<UnityTransport>().SetRelayServerData(sd);
+            NetworkManager.Singleton.StartHost();
+            return joincode;
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError("relay error:" + e);
+            return "";
+        }
+
+    }
+    public async void joinRelay(string joincode)
+    {
+        try
+        {
+            JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(joincode);
+
+            RelayServerData sd = new RelayServerData(allocation, "dtls");
+            GetComponent<UnityTransport>().SetRelayServerData(sd);
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError("relay error:" + e);
+        }
+    }
+}
--- menuFunc.cs
+++ menuFunc.cs
@@ -1,9 +1,51 @@
+using TMPro;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class menuFunc : MonoBehaviour
+public class menuFunc : Singleton< MonoBehaviour>
 {
-    public void Exit(){
+    string joincode;
+    [SerializeField] GameObject menuCam;
+    public async void EnterRoom(GameObject room)
+    {
+        joincode = GameObject.Find("JoinCodeInput").GetComponent<TMPro.TMP_InputField>().text;
+        try
+        {
+            if ((joincode == null) || (joincode == ""))
+            {
+                Debug.Log("Please Enter a Join Code");
+                joincode = await GameObject.FindGameObjectWithTag("net").GetComponent<ConectController>().createRelay();
+                Debug.Log(joincode);
+            }
+            else
+            {
+                GameObject.FindGameObjectWithTag("net").GetComponent<ConectController>().joinRelay(joincode);
 
+            }
+            room.SetActive(true);
+            room.transform.GetChild(2).GetComponent<TMP_Text>().text=joincode;
+            
+        }
+        catch (UnityException e)
+        {
+            Debug.Log(e);
+        }
+
+    }
+
+    public void toogleGameMenu(bool isMenu)
+    {
+        //SceneManager.LoadScene("gameScene");
+        Camera.current.gameObject.transform.GetChild(0).gameObject.SetActive(isMenu);
+        Camera.current.gameObject.transform.GetChild(1).gameObject.SetActive(!isMenu);
+        menuCam.GetComponent<Cinemachine.CinemachineVirtualCamera>().Priority=isMenu?100:0;
+
+    }
+
+
+    public void Exit()
+    {
         Application.Quit();
     }
 }
