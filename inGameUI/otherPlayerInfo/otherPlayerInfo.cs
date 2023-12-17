806616a1b6950d9fefd99e20fdab8ddb18e72278
cygnusDownfall <phapsuhacam112@gmail.com>
2023-12-17 22:33:23 +0700

    %1

--- inGameUI/otherPlayerInfo/otherPlayerInfo.cs
+++ inGameUI/otherPlayerInfo/otherPlayerInfo.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 [RequireComponent(typeof(NetworkObject))]
 public class otherPlayerInfo : SingletonNetwork<otherPlayerInfo>
 {
     public GameObject infoPrefab;
-    public List<playerInfo> datas;
+    public List<playerInfo> datas = new List<playerInfo>();
 
     [ClientRpc]
     public void SpawnInfoClientRpc(Vector3 pos, string name, NetworkObjectReference playerObj)
@@ -13,8 +14,12 @@ public class otherPlayerInfo : SingletonNetwork<otherPlayerInfo>
         var go = Instantiate(infoPrefab, pos, Quaternion.identity);
 
         playerObj.TryGet(out NetworkObject net);
-        //playerInfo info=net.gameObject.GetComponent<ControllReceivingSystem>().curCharacterControl.gameObject
-        datas.Add(null);
+        playerInfo info = net.gameObject.GetComponent<ControllReceivingSystem>().curCharacterControl.gameObject.GetComponent<playerInfo>();
+        info.hp.OnValueChanged += (o, n) =>
+        {
+            go.GetComponentInChildren<Slider>().value = n / (float)info.maxHP;
+        };
+        datas.Add(info);
     }
 
 }
