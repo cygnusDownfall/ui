e9cafb1d0b60bd836fb9af543654ebe8c5aa5da2
cygnusDownfall <phapsuhacam112@gmail.com>
2024-01-03 22:27:02 +0700

    %1

--- inGameUI/otherPlayerInfo/otherPlayerInfo.cs
+++ inGameUI/otherPlayerInfo/otherPlayerInfo.cs
@@ -2,16 +2,27 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+[RequireComponent(typeof(NetworkObject))]
 public class otherPlayerInfo : SingletonNetwork<otherPlayerInfo>
 {
     public GameObject infoPrefab;
     public Dictionary<ulong, Slider> datas = new Dictionary<ulong, Slider>();
 
-    public void SpawnInfo(playerInfo info, ulong clientID)
+    [ServerRpc(RequireOwnership = false)]
+    public void SpawnInfoServerRpc(NetworkBehaviourReference infoRef, ulong clientID)
     {
+        infoRef.TryGet(out playerInfo info);
         var infoObj = itemPooling.Instance.TakeOut("otherinfo") ?? Instantiate(infoPrefab, transform);
+        var infoNet = infoObj.GetComponent<NetworkObject>();
+        infoNet.SpawnWithOwnership(clientID);
+        infoNet.TrySetParent(transform);
+
+        //set 
         infoObj.GetComponentInChildren<TMPro.TMP_Text>().text = "Player " + clientID;
-        datas.Add(clientID, infoObj.GetComponentInChildren<Slider>());
+        var hpUI = infoObj.GetComponentInChildren<Slider>();
+        hpUI.value = 1;
+        datas.Add(clientID, hpUI);
+
         info.hp.OnValueChanged += (v1, v2) =>
         {
             updateValueInfo(v2 / (float)info.maxHP, clientID);
